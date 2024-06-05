3768e36cd6443907df87c275e11b9096cbf54bcc
bajka <masha.bajka@gmail.com>
2024-06-05 16:57:56 +0300

    адаптив

--- src/views/pages/Home.jsx
+++ src/views/pages/Home.jsx
@@ -5,10 +5,16 @@ module.exports = function Home({login}) {
     return (
         <>
     <Layout login={login} >
-<div > 
+<div className='homeNormalSize'> 
     <a className="btn btn-success me-2 p-2" type="button" href='/alltrails' style={{position:'absolute', zIndex:'2', float:'left', top:'20%', left:'30%', right:'30%', backgroundColor:'lightgreen', color:'black', border:'0px'}}>Посмотреть маршруты</a>
  <div style={{position:'relative'}}>
-      <img src="/assets/carousel3.jpg" className="d-block w-100 h-100" alt="лес" />
+      <img src="/assets/home1.jpg" className="d-block w-100 h-100" alt="лес" />
+    </div>
+</div>
+<div className='homePhoneSize'> 
+    <a className="btn btn-success me-2 p-2" type="button" href='/alltrails' style={{position:'absolute', zIndex:'2', float:'left', top:'20%', left:'30%', right:'30%', backgroundColor:'lightgreen', color:'black', border:'0px'}}>Посмотреть маршруты</a>
+ <div style={{position:'relative'}}>
+      <img src="/assets/home2.jpg" className="d-block w-100 h-100" alt="лес" />
     </div>
 </div>
       </Layout>
